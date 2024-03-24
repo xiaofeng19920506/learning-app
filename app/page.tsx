412993a4dc8bbeb4ffccf260d228ba4e0b327485
Aaron <xiaofengliu1992@hotmail.com>
2024-03-24 16:41:51 -0400

    integrate chatbot

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import DeployButton from "../components/DeployButton";
 import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
-import Header from "@/components/Header";
+// import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
+// import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
+// import Header from "@/components/Header";
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
@@ -29,10 +29,10 @@ export default async function Index() {
       </nav>
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Header />
+        {/* <Header /> */}
         <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+          {/* <h2 className="font-bold text-4xl mb-4">Next steps</h2>
+          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />} */}
         </main>
       </div>
 
